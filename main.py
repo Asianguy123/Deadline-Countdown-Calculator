b37721f5fef3e5de267746b85c3f83f2969c1551
Asianguy123 <sas@drsyed.org>
2021-11-14 01:24:37 +0000

    Add files via upload

---
+++ main.py
@@ -0,0 +1,19 @@
+# Calculations + User handling
+
+def user_list():
+    pass
+
+def create_user():
+    pass
+
+def save_user():
+    pass
+
+def login():
+    pass
+
+def update_user():
+    pass
+
+def hour_calc():
+    pass
