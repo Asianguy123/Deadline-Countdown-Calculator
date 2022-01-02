3205c6a809b9fe3bdd13eced7562abad65a8857b
Asianguy123 <sas@drsyed.org>
2022-01-02 21:16:24 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,8 @@
 # Calculations + User handling
 
+import time
 import datetime
 import calendar
-
-def user_list():
-    pass
-
-def create_user():
-    pass
-
-def save_user():
-    pass
-
-def login():
-    pass
-
-def update_user():
-    pass
 
 def time_today():
     wknd = False
@@ -27,7 +13,7 @@ def time_today():
         hours_subbed = current_time.hour + 1
     elif current_time.minute < 30:
         hours_subbed = current_time.hour
-        
+
     if wknd:
         if (24 - hours_subbed) < 10:
             hours = 24 - hours_subbed
@@ -39,6 +25,28 @@ def time_today():
         else:
             hours = 6
     return hours
+    
+def hours_to_work(hours_today, deadline_y, deadline_m, deadline_d):
+    free_hours = hours_today
+    d0 = datetime.datetime.today()
+    d1 = datetime.datetime(deadline_y, deadline_m, deadline_d)
+    delta_days = (d1 - d0).days
+    for i in range(1, delta_days + 1):
+        date = d0 + datetime.timedelta(days=i)
+        if calendar.weekday(date.year, date.month, date.day) >= 5:
+            free_hours += 10
+        elif calendar.weekday(date.year, date.month, date.day) <= 4:
+            free_hours += 6
+    return free_hours
 
-def total_hours():
-    pass
+def main():
+    print('Welcome to Deadline Calculator bestie <3')
+    time.sleep(1)
+    year = input('Enter the deadline year in yyyy format:  ')
+    month = input('Enter the deadline month in mm format:  ')
+    date = input('Enter the deadline date in dd format:  ')
+    working_hours = hours_to_work(time_today(), int(year), int(month), int(date))
+    print(f'You have {working_hours} hours left to complete your assignment')
+
+if __name__ == '__main__':
+    main()
