3d1f666334f309a218ed005691c6231bda60e0cb
Asianguy123 <sas@drsyed.org>
2022-01-02 04:08:59 +0000

    Update main.py

--- main.py
+++ main.py
@@ -22,6 +22,8 @@ def time_left(deadline_y, deadline_m, deadline_d):
     current_time = datetime.datetime.now()
     deadline_time = datetime.datetime(deadline_y, deadline_m, deadline_d)
     remaining_days = (deadline_time - current_time).days
+    remaining_seconds = (deadline_time - current_time).seconds
+    return remaining_days, remaining_seconds
 
 def total_hours():
     pass
