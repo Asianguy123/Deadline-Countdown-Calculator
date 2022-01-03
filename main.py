f78cddf656e13b9a26f2f0b31b90d95e7e763cd8
Asianguy123 <sas@drsyed.org>
2022-01-03 16:50:49 +0000

    Update main.py

--- main.py
+++ main.py
@@ -13,18 +13,24 @@ import calendar
 def time_today():
     wknd = False
     current_time = datetime.datetime.now()
+
+    # checking if current day is a weekend
     if calendar.weekday(current_time.year, current_time.month, current_time.day) >= 5:
         wknd = True
+
+    # accounts for rounding current hour
     if current_time.minute >= 30:
         hours_subbed = current_time.hour + 1
     elif current_time.minute < 30:
         hours_subbed = current_time.hour
 
+    # if today is a weekend, give appropriate remaining hours
     if wknd:
         if (24 - hours_subbed) < 10:
             hours = 24 - hours_subbed
         else:
             hours = 10
+    # if today is a weekday, give appropriate remaining hours
     if not wknd:
         if (24 - hours_subbed) < 6:
             hours = 24 - hours_subbed
@@ -36,7 +42,9 @@ def hours_to_work(hours_today, deadline_y, deadline_m, deadline_d):
     free_hours = hours_today
     d0 = datetime.datetime.today()
     d1 = datetime.datetime(deadline_y, deadline_m, deadline_d)
-    delta_days = (d1 - d0).days
+    delta_days = (d1 - d0).days # number of days from today and deadline date
+
+    # check every day before deadline, to see if its a weekend - add appropriate hours
     for i in range(1, delta_days + 1):
         date = d0 + datetime.timedelta(days=i)
         if calendar.weekday(date.year, date.month, date.day) >= 5:
@@ -56,7 +64,7 @@ def main():
     date = input('Enter the deadline date in dd format:  ')
     working_hours = hours_to_work(time_today(), int(year), int(month), int(date))
     print(f'You have {working_hours} hours left to complete your assignment')
-    exit = input('Calculation complete, hit enter to close:  ')
+    exit = input('Calculation complete, hit enter to close:  ') # so that the shell doesnt close before output can be read
 
 # ------------------------------------------------------------------------------------------------
 # Runs Program
